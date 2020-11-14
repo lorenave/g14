181a96ea992a0ec838e08c0dcbf91347106ff200
lorenave <lorenaverdugoa@gmail.com>
2020-11-13 21:53:44 -0300

    initial commit

---
+++ ricomida/assets/js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var gato = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top
+        }, 800, function(){
+          window.location.hash = gato;
+        });
+      }
+    });
+  
+    $('[data-toggle="popover"]').popover();
+    
+
+    $('subtitulo-receta').on("dblclick", function(){
+        $(this).addClass('rojo');
+    })
+  
+  });
+  
